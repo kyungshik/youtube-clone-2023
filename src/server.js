9b9d43d018305ca4a7474ecdd943964ae2237224
Kyungshik <kyungshikseong@gmail.com>
2023-05-02 13:13:26 +0900

    #3.1,2 GET Requests

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 import express from "express";
+
 const PORT = 4000;
+
 const app = express();
 
+//request : asking, sending something to server
+// const handleHome = () => console.log("Somebody is trying to go home");
+// app.get("/", handleHome);
+app.get("/", () => console.log("Somebody is trying to go home"));
+
+//서비를 외부에 개방
 const handleListening = () => 
-    console.log(`Server Listening on port http://localhost:${PORT} 🐻`);
+    console.log(`✅ Server Listening on port http://localhost:${PORT} ✨`);
+
 app.listen(PORT, handleListening);
 
