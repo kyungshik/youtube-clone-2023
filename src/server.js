41ad6ecb105f154d71f09ccfecc466286a33da79
Kyungshik <kyungshikseong@gmail.com>
2023-05-03 21:16:56 +0900

    #3.5,6 Middlewares
    Definition of Middlewares

--- src/server.js
+++ src/server.js
@@ -4,12 +4,30 @@ const PORT = 4000;
 
 const app = express();
 
-//request : asking, sending something to server
-// const handleHome = () => console.log("Somebody is trying to go home");
-// app.get("/", handleHome);
-app.get("/", () => console.log("Somebody is trying to go home"));
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed, you may continue.");
+    next();
+};
+const handleHome = (req, res, next) => {
+    return res.send("Have a good time :)");
+}
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the privage lounge");
+}
+app.use(logger);
+app.use(privateMiddleware);
 
-//서비를 외부에 개방
+app.get("/", logger, handleHome);
+app.get("/protected", handleProtected);
+
 const handleListening = () => 
     console.log(`✅ Server Listening on port http://localhost:${PORT} ✨`);
 
