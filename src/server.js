a7be05628e84ab6f5a3d3928cb66ffc931b8e7ee
Kyungshik <kyungshikseong@gmail.com>
2023-05-04 17:43:33 +0900

    #3.11 External Middlewares
    install morgan

--- src/server.js
+++ src/server.js
@@ -1,31 +1,26 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed, you may continue.");
-    next();
-};
 const handleHome = (req, res, next) => {
+    console.log("I will respond");
     return res.send("Have a good time :)");
 }
 const handleProtected = (req, res) => {
     return res.send("Welcome to the privage lounge");
 }
-app.use(logger);
-app.use(privateMiddleware);
+const handleLogin = (req, res) => {
+    return res.send("login");
+}
 
-app.get("/", logger, handleHome);
+app.use(logger);
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 app.get("/protected", handleProtected);
 
 const handleListening = () => 
