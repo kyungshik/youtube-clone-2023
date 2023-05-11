b6d6ce4a80ed5d9743d40b7787002b832ad2712d
Kyungshik <kyungshikseong@gmail.com>
2023-05-11 23:06:49 +0900

    #5.8~6.2 Array Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,51 @@
-const fakeUser = {
-    username: "kyungshik",
-    loggedIn: false,
+let videos = [
+    {
+        title: "Saul Good Man Review",
+        rating:5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views:1,
+        id:1,
+    },
+    {
+        title: "Working Mom Diary new trailer",
+        rating:4,
+        comments:3,
+        createdAt: "10 minutes ago",
+        views:12,
+        id:2,
+    },
+    {
+        title: "Breaking Bad trailer",
+        rating:5,
+        comments:7,
+        createdAt: "5 minutes ago",
+        views:18,
+        id:3,
+    },
+    {
+        title: "Wall-E 15min Review",
+        rating:5,
+        comments:22,
+        createdAt: "2 minutes ago",
+        views:59,
+        id:4,
+    },
+];
+
+//main home page
+export const trending = (req, res) => {
+    return res.render("home", {pageTitle: "Home", videos});
 };
 
+//(../videos/userName)
+export const watch = (req, res) =>{
+    const { id } = req.params;
+    const video = videos[id-1];
+    return res.render("watch", {pageTitle:`Watching ${video.title}`, video});
+};   
 
-export const trending = (req, res) => 
-    res.render("home", {pageTitle: "Home", fakeUser});
-export const see = (req, res) =>
-    res.render("watch");
+//(../videos/userName/Edit)
 export const edit = (req, res) => 
     res.render("edit");
 export const search = (req, res) => res.send("Search");
