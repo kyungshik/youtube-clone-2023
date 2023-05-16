d25bf0b5698a50a397b5d283cd43c931325521d3
Kyungshik <kyungshikseong@gmail.com>
2023-05-16 23:24:41 +0900

    #6.20 Edit Video part One
    videoController
    1. watch
    2. getEdit
    3. postEdit
    if statement -> !video : 404 not found

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,19 +12,36 @@ export const home = async(req, res) => {
 export const watch = async (req, res) =>{
     const { id } = req.params;
     const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", {pageTitle:"Video not found."});
+    }
     return res.render("watch", {pageTitle:video.title, video});
 };   
 
 //(../videos/userName/Edit)
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", {pageTitle:`Editing`});
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", {pageTitle:"Video not found."});
+    }
+    return res.render("edit", {pageTitle:`Edit: ${video.title}`, video});
 };
 
 //Change video's title
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const {title, description, hashtags} = req.body;
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", {pageTitle:"Video not found."});
+    }
+    video.title = title;
+    video.description = description;
+    video.hashtags = hashtags
+        .split(",")
+        .map((word) => (word.startsWith('#') ? word: `#${word}`));
+    await video.save();
     return res.redirect(`/videos/${id}`);
 };
 
