35996da161d9d3d677a7bca8fe13646cc0d809f8
Kyungshik <kyungshikseong@gmail.com>
2023-05-18 22:51:52 +0900

    #7.4~6 Status Codes, Log in

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,7 +11,7 @@ export const watch = async (req, res) =>{
     const { id } = req.params;
     const video = await Video.findById(id);
     if(!video){
-        return res.render("404", {pageTitle:"Video not found."});
+        return res.status(404).render("404", {pageTitle:"Video not found."});
     }
     return res.render("watch", {pageTitle:video.title, video});
 };   
@@ -21,7 +21,7 @@ export const getEdit = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
     if(!video){
-        return res.render("404", {pageTitle:"Video not found."});
+        return res.status(404).render("404", {pageTitle:"Video not found."});
     }
     return res.render("edit", {pageTitle:`Edit: ${video.title}`, video});
 };
@@ -58,7 +58,7 @@ export const postUpload = async (req, res) => {
         });
         return res.redirect("/");
     }catch(error){
-        return res.render("upload", {
+        return res.status(400).render("upload", {
             pageTitle: "Upload Video", 
             errorMessage: error._message
         });
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
-import {login, getJoin, postJoin} from "../controllers/userController";
+import {getLogin, postLogin, getJoin, postJoin} from "../controllers/userController";
 import {home, search} from "../controllers/videoController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
+
 rootRouter.get("/search", search);
 
 export default rootRouter;
