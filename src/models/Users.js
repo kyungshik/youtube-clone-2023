6bc7de161336f067f1da5f0fed9ec5512bea7a86
Kyungshik <kyungshikseong@gmail.com>
2023-05-19 01:10:27 +0900

    #7.10 Logged In User part Two

--- src/models/Users.js
+++ src/models/Users.js
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+    name: {type:String, required:true, unique: true},
     email: {type:String, required:true, unique: true},
     username: {type:String, required:true, unique: true},
     password: {type:String, required:true},
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -21,18 +22,7 @@ app.use(
     })
 );
 
-app.use((req,res,next)=>{
-    req.sessionStore.all((error, sessions) => {
-        console.log(sessions);
-        next();
-    })
-})
-
-app.get("/add-one", (req,res,next)=>{
-    req.session.potato += 1;
-    return res.send(`${req.session.id}\n${req.session.potato}`);
-})
-
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
