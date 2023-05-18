aa80ba2a7c96bcf2b38ed00d7ecdc5f8a1ba1e08
Kyungshik <kyungshikseong@gmail.com>
2023-05-18 17:56:01 +0900

    #7.1 Create Account part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,18 @@
+import User from "../models/Users";
+
 export const getJoin = (req, res) => 
     res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
     console.log(req.body);
-    res.end();
+    const {name, username, email, password, location} = req.body;
+    await User.create({
+        name, 
+        username, 
+        email, 
+        password, 
+        location,
+    })
+    return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
