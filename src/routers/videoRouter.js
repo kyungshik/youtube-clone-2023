05287172c0551356a4dd58abc006602ff7d39f37
Kyungshik <kyungshikseong@gmail.com>
2023-05-25 14:08:42 +0900

    #8.2 Edit Profile POST part One

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,26 @@
 import express from "express";
 import {watch, getEdit, postEdit, getUpload, postUpload, deleteVideo} from "../controllers/videoController";
+import {protectedMiddleware, publicOnlyMiddleware} from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo).post(postEdit);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+    .get("/:id([0-9a-f]{24})", watch);
+videoRouter
+    .route("/:id([0-9a-f]{24})/edit")
+    .all(protectedMiddleware)
+    .get(getEdit)
+    .post(postEdit);
+videoRouter
+    .route("/:id([0-9a-f]{24})/delete")
+    .all(protectedMiddleware)
+    .get(deleteVideo)
+    .post(postEdit);
+videoRouter
+    .route("/upload")
+    .all(protectedMiddleware)
+    .get(getUpload)
+    .post(postUpload);
 
 
 export default videoRouter;
