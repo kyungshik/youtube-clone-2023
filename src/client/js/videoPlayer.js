aa1b4d4a5f550d9e62893d65658b0176ecbc0f1e
Kyungshik <kyungshikseong@gmail.com>
2023-05-31 11:46:22 +0900

    #12 VIEWS API

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -51,6 +51,7 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+
 //Time 
 const formatTime = (seconds) =>
   new Date(seconds * 1000).toISOString().substring(14, 19);
@@ -103,11 +104,20 @@ const handleMouseLeave = () => {
   controlsTimeout = setTimeout(hideControls, 3000);
 };
 
+const handleEnded = () => {
+  const { id } = videoContainer.dataset;
+  fetch(`/api/videos/${id}/view`, {
+    method: "POST",
+  });
+};
+
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadeddata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+video.addEventListener("ended", handleEnded);
 videoContainer.addEventListener("mousemove", handleMouseMove);
 videoContainer.addEventListener("mouseleave", handleMouseLeave);
 timeline.addEventListener("input", handleTimelineChange);
