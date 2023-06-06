b4527c988d3f1d700264aa9990fd48b5f9264e2e
Kyungshik <kyungshikseong@gmail.com>
2023-06-06 22:20:58 +0900

    #13 Thumbnails and SCSS

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,25 +1,40 @@
 import express from "express";
-import {watch, getEdit, postEdit, getUpload, postUpload, deleteVideo} from "../controllers/videoController";
-import {protectedMiddleware, publicOnlyMiddleware, videoUpload} from "../middlewares";
+import {
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+  deleteVideo,
+} from "../controllers/videoController";
+import {
+  protectedMiddleware,
+  publicOnlyMiddleware,
+  videoUpload,
+} from "../middlewares";
 
 const videoRouter = express.Router();
 
+videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter
-    .get("/:id([0-9a-f]{24})", watch);
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectedMiddleware)
+  .get(getEdit)
+  .post(postEdit);
 videoRouter
-    .route("/:id([0-9a-f]{24})/edit")
-    .all(protectedMiddleware)
-    .get(getEdit)
-    .post(postEdit);
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectedMiddleware)
+  .get(deleteVideo);
 videoRouter
-    .route("/:id([0-9a-f]{24})/delete")
-    .all(protectedMiddleware)
-    .get(deleteVideo)
-videoRouter
-    .route("/upload")
-    .all(protectedMiddleware)
-    .get(getUpload)
-    .post(videoUpload.single("video"), postUpload);
-
+  .route("/upload")
+  .all(protectedMiddleware)
+  .get(getUpload)
+  .post(
+    videoUpload.fields([
+      { name: "video"},
+      { name: "thumb"},
+    ]),
+    postUpload
+  );
 
 export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -27,6 +27,13 @@ app.use(
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
+
+app.use((req, res, next) => {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+  });
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
